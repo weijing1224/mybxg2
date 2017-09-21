8bef9c8284b9bef94c96ba943f30d116179c5844
weijing <582464224@qq.com>
2017-09-21 14:55:45 +0800

    用模板引擎渲染用户的头像信息

--- public/js/common.js
+++ public/js/common.js
@@ -1,36 +1,48 @@
+define(['jquery','template','cookie'],function($,template){
+  // NProgress.start();
+  // NProgress.done();
 
-NProgress.start();
-NProgress.done();
-
-$('.navs ul').prev('a').on('click', function () {
-	$(this).next().slideToggle();
-});
-
-// 实现退出功能
-$('#logoutBtn').click(function(){
-  $.ajax({
-    type : 'post',
-    url : '/api/logout',
-    dataType : 'json',
-    success : function(data){
-      if(data.code == 200){
-        // 重新跳转到登录页
-        location.href = '/main/login';
-      }
-    }
+  $('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
   });
-});
 
-// 检测用户是否登录
-var flag = $.cookie('PHPSESSID');
-if(!flag){
-  // 如果cookie不存在，跳转到登录页
-  location.href = '/main/login';
-}
+  // 实现退出功能
+  $('#logoutBtn').click(function(){
+    $.ajax({
+      type : 'post',
+      url : '/api/logout',
+      dataType : 'json',
+      success : function(data){
+        if(data.code == 200){
+          // 重新跳转到登录页
+          location.href = '/main/login';
+        }
+      }
+    });
+  });
 
-// 设置用户头像信息
-var loginInfo = $.cookie('loginInfo');
-loginInfo = loginInfo && JSON.parse(loginInfo);
-// 设置用户的头像信息
-$('.aside .profile img').attr('src',loginInfo.tc_avatar);
-$('.aside .profile h4').html(loginInfo.tc_name);
+  // 检测用户是否登录
+  var flag = $.cookie('PHPSESSID');
+  if(!flag && location.pathname !== '/main/login'){
+    // 如果cookie不存在，跳转到登录页
+    location.href = '/main/login';
+  }
+
+  // 设置用户头像信息
+  var loginInfo = $.cookie('loginInfo');
+  loginInfo = loginInfo && JSON.parse(loginInfo);
+  // 设置用户的头像信息
+  // if(flag){
+  //   $('.aside .profile img').attr('src',loginInfo.tc_avatar);
+  //   $('.aside .profile h4').html(loginInfo.tc_name);
+  // }
+  
+    var tpl = '<div class="avatar img-circle">{{tc_avatar}}</div><h4>{{tc_name}}</h4>';
+    // console.log(loginInfo)
+    // 调用方法，进行渲染
+    var html = template.render(tpl,loginInfo);
+    // 添加到上一级元素中
+    $('.aside .profile').html(html);
+  
+
+})
---
+++ public/js/config.js
@@ -0,0 +1,13 @@
+require.config({
+	// 设置引用模块的参考路径
+	baseUrl: '/public/assets',
+	// 给文件设置别名
+	paths: {
+		jquery : 'jquery/jquery',
+		cookie : 'jquery-cookie/jquery.cookie',
+		template : 'artTemplate/template-web',
+		common : '../js/common',
+		login : '../js/login'
+	}
+
+})
